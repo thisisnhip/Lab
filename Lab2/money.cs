962c239673d003bb075f4eb815a53225a9475879
thisisnhip <nhinguyenphuong2005@gmail.com>
2024-10-03 20:59:37 +0700

    add money

---
+++ Lab2/money.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        const int Scale = 23000;
+        Console.WriteLine("Nhap vao so Dollar ma ban co");
+        int usd  = int.Parse(Console.ReadLine());
+        Console.WriteLine($"So tien VND ma ban co la {Scale*usd}");
+    }
+}
