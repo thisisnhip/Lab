7c1c6f2793200b10b036c6003e3e4fa458f39e07
thisisnhip <nhinguyenphuong2005@gmail.com>
2024-10-03 21:02:22 +0700

    Create datetime.cs

---
+++ Lab1/datetime.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap chieu rong hinh chu nhat");
+        float width = float.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap chieu cao hinh chu nhat");
+        float height = float.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Chu vi hinh chu nhat la {(width + height)*2}");
+        Console.WriteLine($"Dien tich hinh chu nhat la {width*height}");
+    }
+}
