e6bba2497138b7d78905da342fc3113366ea86cb
thisisnhip <nhinguyenphuong2005@gmail.com>
2024-10-04 10:03:32 +0700

    Update datetime.cs

--- Lab1/datetime.cs
+++ Lab1/datetime.cs
@@ -2,12 +2,7 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Nhap chieu rong hinh chu nhat");
-        float width = float.Parse(Console.ReadLine());
-        Console.WriteLine("Nhap chieu cao hinh chu nhat");
-        float height = float.Parse(Console.ReadLine());
-
-        Console.WriteLine($"Chu vi hinh chu nhat la {(width + height)*2}");
-        Console.WriteLine($"Dien tich hinh chu nhat la {width*height}");
+    DateTime localDate = DateTime.Now;
+    System.Console.WriteLine("Datetime now is: " + localDate);
     }
 }
