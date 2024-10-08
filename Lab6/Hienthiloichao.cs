cf56418252eb71dba05bf998f6c939f70983e0ad
thisisnhip <nhinguyenphuong2005@gmail.com>
2024-10-08 17:42:34 +0700

    Create Hienthiloichao.cs

---
+++ Lab6/Hienthiloichao.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name: ");
+
+        string yourName = Console.ReadLine();
+
+        Console.WriteLine("Hello: " + yourName);
+    }
+}
