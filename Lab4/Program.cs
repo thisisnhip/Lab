8ecd72b282057bc0286eed52c3c5a75425f29e6f
thisisnhip <nhinguyenphuong2005@gmail.com>
2024-10-08 17:33:35 +0700

    Create thuc hanh su dung toan tu

---
+++ Lab4/Program.cs
@@ -0,0 +1,15 @@
+﻿//[Thực hành] Sử dụng toán tử
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float width;
+        float height;
+        Console.Write("Enter width: ");
+        width = float.Parse(Console.ReadLine());
+        Console.Write("Enter height: ");
+        height = float.Parse(Console.ReadLine());
+        float area = width * height;
+        Console.WriteLine("Area is: " + area);
+    }
+}
